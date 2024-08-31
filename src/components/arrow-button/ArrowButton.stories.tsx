3db683b77bdd6da7a3c3d3ce3278553de657f365
Dmitriy982 <nomad.aleks777@gmail.com>
2024-09-01 02:39:21 +0300

    First try

--- src/components/arrow-button/ArrowButton.stories.tsx
+++ src/components/arrow-button/ArrowButton.stories.tsx
@@ -13,7 +13,7 @@ export const ArrowButtonStory: Story = {
 	render: () => {
 		return (
 			<>
-				<ArrowButton />
+				<ArrowButton onClick={() => alert('Button clicked!')} isOpen={true}/>
 			</>
 		);
 	},
